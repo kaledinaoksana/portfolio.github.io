ace702e3c78e9d4d0a8c648fc823ba3f23ba13be
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-20 17:42:43 +0300

    add footer php

---
+++ blocks/footer.php
@@ -0,0 +1,9 @@
+<head>
+	<?php include 'head.php'; ?>
+</head> 
+
+<body>
+	<footer class="footer text-center py-4">
+		<small class="copyright" style="color: #FFFFFF">2023 </span><i class="bi bi-heart-fill" style="color:#fe655c"></i> </small>
+	</footer>
+</body>
