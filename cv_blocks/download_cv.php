15fb44f20cd195a495a0e56c8b03a1fdef6faa52
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-20 19:45:07 +0300

    change index.php

---
+++ cv_blocks/download_cv.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;url=cv.php">
+        <title>Redirecting...</title>
+        <style>
+            body {
+            background-color: #19202E; 
+            margin: 0; 
+            padding: 0; 
+            }
+        </style>
+    </head>
+    <body>
+    </body>
+</html>
+
+<a href="https://web-studio24.ru/x.pdf" download>Скачать PDF</a>
