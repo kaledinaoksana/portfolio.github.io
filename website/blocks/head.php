e3640b942afe3618a94d268719c78de94f4a2eeb
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-21 00:09:12 +0300

    del meta

--- website/blocks/head.php
+++ website/blocks/head.php
@@ -1,9 +1,5 @@
 <!-- Meta -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Profolio Bootstrap 5 Template">
-<meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+<meta charset="utf-8
 <link rel="shortcut icon" href="/images/favicon.ico"> 
 
 <!-- Google Fonts -->	
