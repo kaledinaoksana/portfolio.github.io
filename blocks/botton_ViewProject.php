135ba22ac1992d569087712d860c6d06de2e009e
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-20 17:43:03 +0300

    add botons

---
+++ blocks/botton_ViewProject.php
@@ -0,0 +1,13 @@
+<head>
+	<?php include 'head.php'; ?>
+</head> 
+
+<body>
+    <div class="actions special d-flex justify-content-center" style="padding: 2% ">
+        <a href="kpmg.html" class="button">
+            <h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
+            <i class="fas fa-eye mr-2"></i>View Project
+            </h3>
+        </a>
+    </div>
+</body>
