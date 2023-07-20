9de95cf54dc3866474b1eaeca2777428c081d49e
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-20 22:33:10 +0300

    add ref for download

--- website/cv_blocks/cv_header.php
+++ website/cv_blocks/cv_header.php
@@ -2,8 +2,14 @@
 <div class="resume-role-title text-uppercase">Data Analyst</div>
 <div class="resume-contact mt-4">
     <ul class="resume-contact-list list-unstyled list-inline mb-0 justify-content-between">
+        
         <li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-telephone-inbound me-2"></i>+7 (911) 114 92 85 </li>
+        
         <li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-envelope me-2"></i>kaledinaoa@gmail.com</li>
-        <li class="list-inline-item me-lg-5"><i class="resume-contact-icon bi bi-globe me-2"></i>Remote, Europe</li>
+        <li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-globe me-2"></i>Remote, Europe</li>
+        <a href="/website/docs/KaledinaOA_CV.pdf" download style="color: #A5B3CE">
+        <li class="list-inline-item me-md-3 me-lg-5"><i class="resume-contact-icon bi bi-file-earmark-pdf"></i>CV</li>
+        </a>
+        
     </ul>
 </div><!--//resume-contact-->
