807c4cb70547bbbfef0123f53a181b436722186c
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-20 20:15:46 +0300

    change locations

--- website/articles/covid_art.php
+++ website/articles/covid_art.php
@@ -8,7 +8,7 @@
     </div>
 
     <div class="actions special d-flex justify-content-center" style="padding: 2% ">
-    <a href="../pages/covid.php" class="button">
+    <a href="/website/pages/covid.php" class="button">
         <h3 class="resume-section-heading text-uppercase py-2 py-lg-3 py-3">
         <i class="fas fa-eye mr-2"></i>View Project
         </h3>
