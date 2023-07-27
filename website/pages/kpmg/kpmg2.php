a51ab91c8b8ad90d0e77d19dba771bacf1e522f3
Oksana Kaledina <kaledinaoa@gmail.com>
2023-07-28 01:24:36 +0400

    add images about corr kpmg

--- website/pages/kpmg/kpmg2.php
+++ website/pages/kpmg/kpmg2.php
@@ -7,5 +7,11 @@
 
 <p><b>Corellation</b></p>
 <p> 'age' and 'tenture' = 0.45</p>
+<a class="image fit" style="padding: 0% 5% 0% 5%;" href="https://public.tableau.com/app/profile/oksana.kaledina/viz/Corr_st_cost_and_price/crage">
+    <img src="../../../website/images/cr_age.png" alt="" />
+</a>
 <p> 'list_price' and 'standard_cost' = 0.55</p>
+<a class="image fit" style="padding: 0% 5% 0% 5%;" href="https://public.tableau.com/app/profile/oksana.kaledina/viz/Corr_16904917240440/crpr?publish=yes">
+    <img src="../../../website/images/cr_pr.png" alt="" />
+</a>
 <!-- <a class="image main"><img src="images/corr.png" alt="" /></a> -->
